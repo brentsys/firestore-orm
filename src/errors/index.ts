6500b020fd4460b08fc7d876bb36630086d53cec
Henri Sack <dev@bretnsys.com>
2022-12-09 17:09:40 +0100

    added declarations

---
+++ src/errors/index.ts
@@ -0,0 +1,2 @@
+export * from './auth_error'
+export * from './no_auth_error'
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export * from './model/record_model';
+export * from './controller'
+export * from './config'
+export * from './constants'
+export * from './repository'
+export * from './tools'
