a4d8bb836879f63a61c36be5c362805f6fd1040f
Henri Sack <dev@bretnsys.com>
2022-12-28 19:09:47 +0100

    added contitional accept-encoding

--- src/repository/rest_repository.ts
+++ src/repository/rest_repository.ts
@@ -20,11 +20,13 @@ export type RestDefinition = ModelDefinition & { settings: ModelSettings & { res
 export abstract class RestRepository<T extends ModelType> extends BaseRepository<T> {
   definition: RestDefinition
   rest: AxiosInstance
+  acceptEncoding = true
 
   constructor(definition: RestDefinition) {
     super()
     this.definition = definition
     const headers: RawAxiosHeaders = definition.settings.restApi.headers ?? {}
+    if (this.acceptEncoding) headers["Accept-Encoding"] = "gzip,deflate,compress"
     const restConfig = {
       baseURL: definition.settings.restApi.baseUrl,
       headers,
